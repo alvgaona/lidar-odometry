5a5cd40abe902d1018a20d350638aecdff473de0
Alvaro Gaona <alvgaona@gmail.com>
2026-01-29 22:35:09 +0100

    feat: add mocap ground truth support to message_converter
    - Add mocap rigid bodies subscriber for ground truth pose
    - Add ground truth pose and path publishers
    - Add TF broadcaster for map to odom transform
    - Use params builder for configurable parameters
    - Add --help support for parameter documentation
    
    Parameters:
    - rigid_body_index: index of rigid body to track
    - odom_topic: odometry input topic
    - mocap_topic: mocap rigid bodies input topic
    - odom_frame: frame ID for odometry
    - map_frame: frame ID for map
    - tf_rate: TF broadcast rate in Hz

--- lidarodom/include/lidarodom/message_converter.hpp
+++ lidarodom/include/lidarodom/message_converter.hpp
@@ -1,23 +1,50 @@
 #pragma once
 
+#include <geometry_msgs/msg/detail/pose_stamped__struct.hpp>
+#include <mocap4r2_msgs/msg/detail/rigid_bodies__struct.hpp>
+#include <mocap4r2_msgs/msg/rigid_bodies.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 #include <nav_msgs/msg/path.hpp>
 #include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <builtin_interfaces/msg/time.hpp>
+#include <tf2_ros/transform_broadcaster.h>
+#include <lidarodom/params.hpp>
 #include <memory>
 
 
 class MessageConverter final : public rclcpp::Node {
 public:
-    MessageConverter();
+    explicit MessageConverter(lidarodom::Params params);
     ~MessageConverter() override = default;
 
 private:
+    lidarodom::Params params_;
+
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscriber_;
+    rclcpp::Subscription<mocap4r2_msgs::msg::RigidBodies>::SharedPtr rigid_bodies_subscriber_;
+
     rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_publisher_;
     rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_publisher_;
 
-    void publish_pose_and_path(const nav_msgs::msg::Odometry::SharedPtr msg);
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr ground_truth_pose_publisher_;
+    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr ground_truth_path_publisher_;
 
     std::unique_ptr<nav_msgs::msg::Path> path_msg_;
+    std::unique_ptr<nav_msgs::msg::Path> ground_truth_path_msg_;
+
+    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+    geometry_msgs::msg::TransformStamped map_to_odom_transform_;
+    bool transform_initialized_ = false;
+
+    rclcpp::TimerBase::SharedPtr tf_timer_;
+
+    void publish_pose_and_path(const nav_msgs::msg::Odometry::SharedPtr msg);
+
+    void publish_rigid_bodies_path(const geometry_msgs::msg::PoseStamped pose);
+
+    geometry_msgs::msg::PoseStamped publish_rigid_bodies_pose(const mocap4r2_msgs::msg::RigidBodies::SharedPtr msg);
+
+    void publish_map_to_odom_transform(const builtin_interfaces::msg::Time& stamp);
 };
--- lidarodom/src/message_converter.cpp
+++ lidarodom/src/message_converter.cpp
@@ -1,33 +1,125 @@
 #include "lidarodom/message_converter.hpp"
+#include <geometry_msgs/msg/detail/pose_stamped__struct.hpp>
+#include <mocap4r2_msgs/mocap4r2_msgs/msg/rigid_bodies.hpp>
+#include <mocap4r2_msgs/msg/detail/rigid_bodies__struct.hpp>
+#include <nav_msgs/msg/detail/path__struct.hpp>
 
-MessageConverter::MessageConverter()
+MessageConverter::MessageConverter(lidarodom::Params params)
     : Node("message_converter"),
-      path_msg_(std::make_unique<nav_msgs::msg::Path>()) {
+      params_(std::move(params)),
+      path_msg_(std::make_unique<nav_msgs::msg::Path>()),
+      ground_truth_path_msg_(std::make_unique<nav_msgs::msg::Path>()),
+      tf_broadcaster_(std::make_unique<tf2_ros::TransformBroadcaster>(*this)) {
 
+    params_.parse(this);
+
+    auto odom_topic = params_.get<std::string>("odom_topic");
+    auto mocap_topic = params_.get<std::string>("mocap_topic");
+    auto odom_frame = params_.get<std::string>("odom_frame");
+    auto map_frame = params_.get<std::string>("map_frame");
+    auto tf_rate = params_.get<double>("tf_rate");
+
+    // Subscribers
     odom_subscriber_ = this->create_subscription<nav_msgs::msg::Odometry>(
-        "/kiss/odometry",
+        odom_topic,
         10,
         [this](const nav_msgs::msg::Odometry::SharedPtr msg) {
-            this->publish_pose_and_path(msg);            
+            this->publish_pose_and_path(msg);
         }
     );
 
+    rigid_bodies_subscriber_ = this->create_subscription<mocap4r2_msgs::msg::RigidBodies>(
+        mocap_topic,
+        10,
+        [this](const mocap4r2_msgs::msg::RigidBodies::SharedPtr msg) {
+            auto pose = this->publish_rigid_bodies_pose(msg);
+            this->publish_rigid_bodies_path(pose);
+        }
+    );
+
+    RCLCPP_INFO(this->get_logger(), "Subscribers:");
+    RCLCPP_INFO(this->get_logger(), "  - %s", odom_topic.c_str());
+    RCLCPP_INFO(this->get_logger(), "  - %s", mocap_topic.c_str());
+
+    // Publishers
     pose_publisher_ = this->create_publisher<geometry_msgs::msg::PoseStamped>(
-        "/kiss/pose",
+        "/lidarodom/pose",
         10
     );
 
     path_publisher_ = this->create_publisher<nav_msgs::msg::Path>(
-        "/kiss/path",
+        "/lidarodom/path",
         10
     );
 
-    // Initialize path frame_id
-    path_msg_->header.frame_id = "odom_lidar";
+    ground_truth_pose_publisher_ = this->create_publisher<geometry_msgs::msg::PoseStamped>(
+        "/lidarodom/ground_truth/pose",
+        10
+    );
 
-    RCLCPP_INFO(this->get_logger(), "Odometry to Pose/Path converter started");
-    RCLCPP_INFO(this->get_logger(), "Subscribing to: /kiss/odometry");
-    RCLCPP_INFO(this->get_logger(), "Publishing to: /kiss/pose and /kiss/path");
+    ground_truth_path_publisher_ = this->create_publisher<nav_msgs::msg::Path>(
+        "/lidarodom/ground_truth/path",
+        10
+    );
+
+    RCLCPP_INFO(this->get_logger(), "Publishers:");
+    RCLCPP_INFO(this->get_logger(), "  - /lidarodom/pose");
+    RCLCPP_INFO(this->get_logger(), "  - /lidarodom/path");
+    RCLCPP_INFO(this->get_logger(), "  - /lidarodom/ground_truth/pose");
+    RCLCPP_INFO(this->get_logger(), "  - /lidarodom/ground_truth/path");
+
+    // Initialize path frame_id
+    path_msg_->header.frame_id = odom_frame;
+    ground_truth_path_msg_->header.frame_id = map_frame;
+
+    map_to_odom_transform_.header.frame_id = map_frame;
+    map_to_odom_transform_.child_frame_id = odom_frame;
+    map_to_odom_transform_.transform.translation.x = 0.0;
+    map_to_odom_transform_.transform.translation.y = 0.0;
+    map_to_odom_transform_.transform.translation.z = 0.0;
+    map_to_odom_transform_.transform.rotation.x = 0.0;
+    map_to_odom_transform_.transform.rotation.y = 0.0;
+    map_to_odom_transform_.transform.rotation.z = 0.0;
+    map_to_odom_transform_.transform.rotation.w = 1.0;
+
+    int timer_period_ms = static_cast<int>(1000.0 / tf_rate);
+    tf_timer_ = this->create_wall_timer(
+        std::chrono::milliseconds(timer_period_ms),
+        [this]() {
+            map_to_odom_transform_.header.stamp = this->now();
+            tf_broadcaster_->sendTransform(map_to_odom_transform_);
+        }
+    );
+}
+
+void MessageConverter::publish_rigid_bodies_path(const geometry_msgs::msg::PoseStamped pose) {
+    ground_truth_path_msg_->poses.push_back(pose);
+    ground_truth_path_msg_->header = pose.header;
+
+    ground_truth_path_publisher_->publish(*ground_truth_path_msg_);
+};
+
+geometry_msgs::msg::PoseStamped MessageConverter::publish_rigid_bodies_pose(const mocap4r2_msgs::msg::RigidBodies::SharedPtr msg) {
+    auto pose_stamped = geometry_msgs::msg::PoseStamped();
+    pose_stamped.pose = msg->rigidbodies.at(params_.get<int>("rigid_body_index")).pose;
+    pose_stamped.header = msg->header;
+
+    if (!transform_initialized_) {
+        map_to_odom_transform_.transform.translation.x = pose_stamped.pose.position.x;
+        map_to_odom_transform_.transform.translation.y = pose_stamped.pose.position.y;
+        map_to_odom_transform_.transform.translation.z = pose_stamped.pose.position.z;
+        map_to_odom_transform_.transform.rotation = pose_stamped.pose.orientation;
+        transform_initialized_ = true;
+    }
+
+    ground_truth_pose_publisher_->publish(pose_stamped);
+
+    return pose_stamped;
+}
+
+void MessageConverter::publish_map_to_odom_transform(const builtin_interfaces::msg::Time& stamp) {
+    map_to_odom_transform_.header.stamp = stamp;
+    tf_broadcaster_->sendTransform(map_to_odom_transform_);
 }
 
 void MessageConverter::publish_pose_and_path(const nav_msgs::msg::Odometry::SharedPtr msg) {
@@ -36,7 +128,6 @@ void MessageConverter::publish_pose_and_path(const nav_msgs::msg::Odometry::Shar
     pose_stamped.pose = msg->pose.pose;
     pose_publisher_->publish(pose_stamped);
 
-    // Add to path and publish
     path_msg_->poses.push_back(pose_stamped);
     path_msg_->header.stamp = msg->header.stamp;
     path_msg_->header.frame_id = msg->header.frame_id;
@@ -44,8 +135,41 @@ void MessageConverter::publish_pose_and_path(const nav_msgs::msg::Odometry::Shar
 }
 
 int main(int argc, char** argv) {
+    lidarodom::Params params;
+
+    params.bind<int>("rigid_body_index")
+        .value(1)
+        .help("Index of the rigid body to track")
+        .range(0, 100);
+
+    params.bind<std::string>("odom_topic")
+        .value("/kiss/odometry")
+        .help("Topic for odometry input");
+
+    params.bind<std::string>("mocap_topic")
+        .value("/mocap/rigid_bodies")
+        .help("Topic for mocap rigid bodies input");
+
+    params.bind<std::string>("odom_frame")
+        .value("odom_lidar")
+        .help("Frame ID for odometry");
+
+    params.bind<std::string>("map_frame")
+        .value("map")
+        .help("Frame ID for map");
+
+    params.bind<double>("tf_rate")
+        .value(50.0)
+        .help("TF broadcast rate in Hz")
+        .range(1.0, 1000.0);
+
+    if (lidarodom::Params::has_help_flag(argc, argv)) {
+        params.print_help("Convert mocap and odometry messages", "lidarodom", "message_converter");
+        return 0;
+    }
+
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<MessageConverter>();
+    auto node = std::make_shared<MessageConverter>(std::move(params));
 
     try {
         rclcpp::spin(node);
